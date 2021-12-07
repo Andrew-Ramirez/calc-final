ca2cbc03a1a3dd88bfad7b177887295e972cb2cb
Andrew Ramirez <alramir3@uci.edu>
2021-12-07 15:03:57 -0800

    actual commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Calc from "./calcComponents/Calc";
+import Display from "./calcComponents/Display";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      result: "",
+    };
+  }
+
+  onClick = (button) => {
+    if (button === "=") {
+      this.eval();
+    }
+
+    else if (button === "Clear") {
+      this.clear();
+    }
+
+    else if (button === "del") {
+      this.del();
+    } 
+    
+    else {
+      this.setState({
+        result: this.state.result + button,
+      });
+    }
+  };
+
+  eval = () => {
+    var solution = this.state.result;
+
+    this.setState({
+      result: eval(solution),
+    });
+  };
+
+  clear = () => {
+    this.setState({
+      result: "",
+    });
+  };
+  del = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1),
+    });
+  };
+
+  render() {
+    return (
+      <div className = "test">
+        <h1>I did it maybe idk</h1>
+        <Display result={this.state.result} />
+        <Calc onClick={this.onClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Random.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Random extends Component {
+    render() {
+        return(
+            <div className = "lol">
+                <p>Yeah I routed something I guess</p>
+            </div>
+        )
+    }
+}
+
+export default Random
